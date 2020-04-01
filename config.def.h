b40b40240edd9f6a697000f6485cdebcbefdba94
Fuseteam <fusekai@outlook.com>
2020-04-01 13:51:33 -0300

    add hightlighting + optimize theming

--- config.def.h
+++ config.def.h
@@ -10,7 +10,9 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSel] = { "#eeeeee", "#228b22" },
+	[SchemeSelHighlight] = { "#ffc978", "#228b22" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
